be2476aa7bd5a9403702df31addf3958d58ff7bf
Brian Vu <emailbrian.vu@gmail.com>
2023-05-31 22:37:07 -0700

    chore: rename variables to orb standards

--- src/scripts/create-application.sh
+++ src/scripts/create-application.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 ORB_EVAL_APPLICATION_NAME="$(eval echo "${ORB_EVAL_APPLICATION_NAME}")"
 ORB_EVAL_REGION="$(eval echo "${ORB_EVAL_REGION}")"
+ORB_EVAL_PROFILE_NAME="$(eval echo "${ORB_EVAL_PROFILE_NAME}")"
+ORB_EVAL_ARGUMENTS="$(eval echo "${ORB_EVAL_ARGUMENTS}")"
 
 set +e
 aws deploy get-application \
-    --application-name "${ORB_EVAL_APPLICATION_NAME}" --profile "${ORB_VAL_PROFILE_NAME}" --region "${ORB_EVAL_REGION}" \
-    "${ORB_VAL_ARGUMENTS}" 
+    --application-name "${ORB_EVAL_APPLICATION_NAME}" --profile "${ORB_EVAL_PROFILE_NAME}" --region "${ORB_EVAL_REGION}" \
+    "${ORB_EVAL_ARGUMENTS}" 
 if $? -ne 0; then
     set -e
     echo "No application named ${ORB_EVAL_APPLICATION_NAME} found. Trying to create a new one"
--- src/scripts/push-bundle.sh
+++ src/scripts/push-bundle.sh
@@ -3,14 +3,17 @@ ORB_EVAL_APPLICATION_NAME="$(eval echo "${ORB_EVAL_APPLICATION_NAME}")"
 ORB_EVAL_BUNDLE_BUCKET="$(eval echo "${ORB_EVAL_BUNDLE_BUCKET}")"
 ORB_EVAL_BUNDLE_KEY="$(eval echo "${ORB_EVAL_BUNDLE_KEY}")"
 ORB_EVAL_REGION="$(eval echo "${ORB_EVAL_REGION}")"
+ORB_EVAL_ARGUMENTS="$(eval echo "${ORB_EVAL_ARGUMENTS}")"
+ORB_EVAL_PROFILE_NAME="$(eval echo "${ORB_EVAL_PROFILE_NAME}")"
+ORB_EVAL_BUNDLE_SOURCE="$(eval echo "${ORB_EVAL_BUNDLE_SOURCE}")"
 
-if [ -n "${ORB_VAL_ARGUMENTS}" ]; then 
-    set -- "$@" "${ORB_VAL_ARGUMENTS}"
+if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then 
+    set -- "$@" "${ORB_EVAL_ARGUMENTS}"
 fi 
 
 aws deploy push \
     --application-name "${ORB_EVAL_APPLICATION_NAME}" \
-    --source "${ORB_VAL_BUNDLE_SOURCE}" \
-    --profile "${ORB_VAL_PROFILE_NAME}" \
+    --source "${ORB_EVAL_BUNDLE_SOURCE}" \
+    --profile "${ORB_EVAL_PROFILE_NAME}" \
     --region "${ORB_EVAL_REGION}" \
     --s3-location s3://"${ORB_EVAL_BUNDLE_BUCKET}/${ORB_EVAL_BUNDLE_KEY}.${ORB_VAL_BUNDLE_TYPE}" "$@"
