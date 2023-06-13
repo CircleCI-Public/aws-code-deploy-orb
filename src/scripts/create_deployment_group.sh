c4e58eb4e37892a8433353ed3769990bd9913255
Brian Vu <emailbrian.vu@gmail.com>
2023-06-12 18:24:09 -0700

    chore: update variable names

--- src/scripts/create_deployment_group.sh
+++ src/scripts/create_deployment_group.sh
@@ -1,28 +1,28 @@
 #!/bin/sh
-ORB_EVAL_APPLICATION_NAME="$(circleci env subst "${ORB_EVAL_APPLICATION_NAME}")"
-ORB_EVAL_DEPLOYMENT_GROUP="$(circleci env subst "${ORB_EVAL_DEPLOYMENT_GROUP}")"
-ORB_EVAL_REGION="$(circleci env subst "${ORB_EVAL_REGION}")"
-ORB_EVAL_ARGUMENTS="$(circleci env subst "${ORB_EVAL_ARGUMENTS}")"
-ORB_EVAL_PROFILE_NAME="$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")"
-ORB_EVAL_SERVICE_ROLE_ARN="$(circleci env subst "${ORB_EVAL_SERVICE_ROLE_ARN}")"
+ORB_STR_APPLICATION_NAME="$(circleci env subst "${ORB_STR_APPLICATION_NAME}")"
+ORB_STR_DEPLOYMENT_GROUP="$(circleci env subst "${ORB_STR_DEPLOYMENT_GROUP}")"
+ORB_STR_REGION="$(circleci env subst "${ORB_STR_REGION}")"
+ORB_STR_ARGUMENTS="$(circleci env subst "${ORB_STR_ARGUMENTS}")"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
+ORB_STR_SERVICE_ROLE_ARN="$(circleci env subst "${ORB_STR_SERVICE_ROLE_ARN}")"
 
 set +e
 aws deploy get-deployment-group \
---application-name "${ORB_EVAL_APPLICATION_NAME}" \
---deployment-group-name "${ORB_EVAL_DEPLOYMENT_GROUP}" \
---region "${ORB_EVAL_REGION}" \
---profile "${ORB_EVAL_PROFILE_NAME}" "${ORB_EVAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}"
+--application-name "${ORB_STR_APPLICATION_NAME}" \
+--deployment-group-name "${ORB_STR_DEPLOYMENT_GROUP}" \
+--region "${ORB_STR_REGION}" \
+--profile "${ORB_STR_PROFILE_NAME}" "${ORB_STR_GET_DEPLOYMENT_GROUP_ARGUMENTS}"
 
 if $? -ne 0; then
   set -e
-  echo "No deployment group named ${ORB_EVAL_DEPLOYMENT_GROUP} found. Trying to create a new one"
+  echo "No deployment group named ${ORB_STR_DEPLOYMENT_GROUP} found. Trying to create a new one"
   aws deploy create-deployment-group \
-  --application-name  "${ORB_EVAL_APPLICATION_NAME}" \
-  --deployment-group-name "${ORB_EVAL_DEPLOYMENT_GROUP}" \
-  --deployment-config-name "${ORB_VAL_DEPLOYMENT_CONFIG}" \
-    --service-role-arn "${ORB_EVAL_SERVICE_ROLE_ARN}" "${ORB_EVAL_ARGUMENTS}"
+  --application-name  "${ORB_STR_APPLICATION_NAME}" \
+  --deployment-group-name "${ORB_STR_DEPLOYMENT_GROUP}" \
+  --deployment-config-name "${ORB_STR_DEPLOYMENT_CONFIG}" \
+    --service-role-arn "${ORB_STR_SERVICE_ROLE_ARN}" "${ORB_STR_ARGUMENTS}"
 else
   set -e
-  echo "Deployment group named ${ORB_EVAL_DEPLOYMENT_GROUP} already exists. Skipping creation."
+  echo "Deployment group named ${ORB_STR_DEPLOYMENT_GROUP} already exists. Skipping creation."
 fi
 
--- src/scripts/deploy_bundle.sh
+++ src/scripts/deploy_bundle.sh
@@ -1,45 +1,45 @@
 #!/bin/sh
-ORB_EVAL_APPLICATION_NAME="$(circleci env subst "${ORB_EVAL_APPLICATION_NAME}")"
-ORB_EVAL_DEPLOYMENT_GROUP="$(circleci env subst "${ORB_EVAL_DEPLOYMENT_GROUP}")"
-ORB_EVAL_BUNDLE_BUCKET="$(circleci env subst "${ORB_EVAL_BUNDLE_BUCKET}")"
-ORB_EVAL_BUNDLE_KEY="$(circleci env subst "${ORB_EVAL_BUNDLE_KEY}")"
-ORB_EVAL_REGION="$(circleci env subst "${ORB_EVAL_REGION}")"
-ORB_EVAL_GET_DEPLOYMENT_GROUP_ARGUMENTS="$(circleci env subst "${ORB_EVAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}")"
-ORB_EVAL_BUNDLE_TYPE="$(circleci env subst "${ORB_EVAL_BUNDLE_TYPE}")"
-ORB_EVAL_DEPLOY_BUNDLE_ARGUMENTS="$(circleci env subst "${ORB_EVAL_DEPLOY_BUNDLE_ARGUMENTS}")"
+ORB_STR_APPLICATION_NAME="$(circleci env subst "${ORB_STR_APPLICATION_NAME}")"
+ORB_STR_DEPLOYMENT_GROUP="$(circleci env subst "${ORB_STR_DEPLOYMENT_GROUP}")"
+ORB_STR_BUNDLE_BUCKET="$(circleci env subst "${ORB_STR_BUNDLE_BUCKET}")"
+ORB_STR_BUNDLE_KEY="$(circleci env subst "${ORB_STR_BUNDLE_KEY}")"
+ORB_STR_REGION="$(circleci env subst "${ORB_STR_REGION}")"
+ORB_STR_GET_DEPLOYMENT_GROUP_ARGUMENTS="$(circleci env subst "${ORB_STR_GET_DEPLOYMENT_GROUP_ARGUMENTS}")"
+ORB_STR_BUNDLE_TYPE="$(circleci env subst "${ORB_STR_BUNDLE_TYPE}")"
+ORB_STR_DEPLOY_BUNDLE_ARGUMENTS="$(circleci env subst "${ORB_STR_DEPLOY_BUNDLE_ARGUMENTS}")"
 
-if [ -n "${ORB_EVAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}" ]; then 
-  set -- "$@" "${ORB_EVAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}"
+if [ -n "${ORB_STR_GET_DEPLOYMENT_GROUP_ARGUMENTS}" ]; then 
+  set -- "$@" "${ORB_STR_GET_DEPLOYMENT_GROUP_ARGUMENTS}"
 fi
 
-if [ -n "${ORB_EVAL_DEPLOY_BUNDLE_ARGUMENTS}" ]; then
-  set -- "$@" "${ORB_EVAL_DEPLOY_BUNDLE_ARGUMENTS}"
+if [ -n "${ORB_STR_DEPLOY_BUNDLE_ARGUMENTS}" ]; then
+  set -- "$@" "${ORB_STR_DEPLOY_BUNDLE_ARGUMENTS}"
 fi 
 
 ID=$(aws deploy create-deployment \
-    --application-name "${ORB_EVAL_APPLICATION_NAME}" \
-    --deployment-group-name "${ORB_EVAL_DEPLOYMENT_GROUP}" \
-    --deployment-config-name "${ORB_VAL_DEPLOYMENT_CONFIG}" \
-    --region "${ORB_EVAL_REGION}" \
-    --s3-location bucket="${ORB_EVAL_BUNDLE_BUCKET}",bundleType="${ORB_EVAL_BUNDLE_TYPE}",key="${ORB_EVAL_BUNDLE_KEY}"."${ORB_EVAL_BUNDLE_TYPE}" \
-    --profile "${ORB_EVAL_PROFILE_NAME}" \
+    --application-name "${ORB_STR_APPLICATION_NAME}" \
+    --deployment-group-name "${ORB_STR_DEPLOYMENT_GROUP}" \
+    --deployment-config-name "${ORB_STR_DEPLOYMENT_CONFIG}" \
+    --region "${ORB_STR_REGION}" \
+    --s3-location bucket="${ORB_STR_BUNDLE_BUCKET}",bundleType="${ORB_STR_BUNDLE_TYPE}",key="${ORB_STR_BUNDLE_KEY}"."${ORB_STR_BUNDLE_TYPE}" \
+    --profile "${ORB_STR_PROFILE_NAME}" \
     --output text \
-    --query '[deploymentId]' "${ORB_EVAL_DEPLOY_BUNDLE_ARGUMENTS}")
+    --query '[deploymentId]' "${ORB_STR_DEPLOY_BUNDLE_ARGUMENTS}")
 STATUS=$(aws deploy get-deployment \
     --deployment-id "$ID" \
     --output text \
-    --profile "${ORB_EVAL_PROFILE_NAME}" \
-    --region "${ORB_EVAL_REGION}" \
+    --profile "${ORB_STR_PROFILE_NAME}" \
+    --region "${ORB_STR_REGION}" \
     --query '[deploymentInfo.status]' \
-    "${ORB_EVAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}")
+    "${ORB_STR_GET_DEPLOYMENT_GROUP_ARGUMENTS}")
 while [ "$STATUS" = "Created" ] || [ "$STATUS" = "InProgress" ] || [ "$STATUS" = "Pending" ] || [ "$STATUS" = "Queued" ] || [ "$STATUS" = "Ready" ]; do
   echo "Status: $STATUS..."
   STATUS=$(aws deploy get-deployment \
             --deployment-id "$ID" \
-            --profile "${ORB_EVAL_PROFILE_NAME}" \
-            --region "${ORB_EVAL_REGION}" \
+            --profile "${ORB_STR_PROFILE_NAME}" \
+            --region "${ORB_STR_REGION}" \
             --output text \
-            --query '[deploymentInfo.status]'"${ORB_EVAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}")
+            --query '[deploymentInfo.status]'"${ORB_STR_GET_DEPLOYMENT_GROUP_ARGUMENTS}")
   sleep 5
 done
 if [ "$STATUS" = "Succeeded" ]; then
@@ -50,8 +50,8 @@ else
   echo "Deployment failed!"
 fi
 aws deploy get-deployment --deployment-id "$ID" \
-"${ORB_EVAL_GET_DEPLOYMENT_GROUP_ARGUMENTS}" \
---profile "${ORB_EVAL_PROFILE_NAME}" \
---region "${ORB_EVAL_REGION}" \
---profile "${ORB_EVAL_PROFILE_NAME}"
+"${ORB_STR_GET_DEPLOYMENT_GROUP_ARGUMENTS}" \
+--profile "${ORB_STR_PROFILE_NAME}" \
+--region "${ORB_STR_REGION}" \
+--profile "${ORB_STR_PROFILE_NAME}"
 exit $EXITCODE
