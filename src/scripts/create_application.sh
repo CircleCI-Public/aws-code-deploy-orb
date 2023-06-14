fe662f10b5c450316e4e206efed84269c8e38dd9
Brian Vu <64455338+brivu@users.noreply.github.com>
2023-06-14 11:59:00 -0700

    refactor: v4.0 (#37)
    * chore: update orb versions
    
    * chore: update variable names
    
    * refactor: job image with executor
    
    * refactor: update region

--- src/scripts/create_application.sh
+++ src/scripts/create_application.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
-ORB_EVAL_APPLICATION_NAME="$(circleci env subst "${ORB_EVAL_APPLICATION_NAME}")"
-ORB_EVAL_REGION="$(circleci env subst "${ORB_EVAL_REGION}")"
-ORB_EVAL_PROFILE_NAME="$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")"
-ORB_EVAL_ARGUMENTS="$(circleci env subst "${ORB_EVAL_ARGUMENTS}")"
+ORB_STR_APPLICATION_NAME="$(circleci env subst "${ORB_STR_APPLICATION_NAME}")"
+ORB_STR_REGION="$(circleci env subst "${ORB_STR_REGION}")"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
+ORB_STR_ARGUMENTS="$(circleci env subst "${ORB_STR_ARGUMENTS}")"
 
 set +e
 aws deploy get-application \
-    --application-name "${ORB_EVAL_APPLICATION_NAME}" --profile "${ORB_EVAL_PROFILE_NAME}" --region "${ORB_EVAL_REGION}" \
-    "${ORB_EVAL_ARGUMENTS}" 
+    --application-name "${ORB_STR_APPLICATION_NAME}" --profile "${ORB_STR_PROFILE_NAME}" --region "${ORB_STR_REGION}" \
+    "${ORB_STR_ARGUMENTS}" 
 if $? -ne 0; then
     set -e
-    echo "No application named ${ORB_EVAL_APPLICATION_NAME} found. Trying to create a new one"
+    echo "No application named ${ORB_STR_APPLICATION_NAME} found. Trying to create a new one"
 else
     set -e
-    echo "Application named ${ORB_EVAL_APPLICATION_NAME} already exists. Skipping creation."
+    echo "Application named ${ORB_STR_APPLICATION_NAME} already exists. Skipping creation."
 fi
--- src/scripts/push_bundle.sh
+++ src/scripts/push_bundle.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
-ORB_EVAL_APPLICATION_NAME="$(circleci env subst "${ORB_EVAL_APPLICATION_NAME}")"
-ORB_EVAL_BUNDLE_BUCKET="$(circleci env subst "${ORB_EVAL_BUNDLE_BUCKET}")"
-ORB_EVAL_BUNDLE_KEY="$(circleci env subst "${ORB_EVAL_BUNDLE_KEY}")"
-ORB_EVAL_REGION="$(circleci env subst "${ORB_EVAL_REGION}")"
-ORB_EVAL_ARGUMENTS="$(circleci env subst "${ORB_EVAL_ARGUMENTS}")"
-ORB_EVAL_PROFILE_NAME="$(circleci env subst "${ORB_EVAL_PROFILE_NAME}")"
-ORB_EVAL_BUNDLE_SOURCE="$(circleci env subst "${ORB_EVAL_BUNDLE_SOURCE}")"
+ORB_STR_APPLICATION_NAME="$(circleci env subst "${ORB_STR_APPLICATION_NAME}")"
+ORB_STR_BUNDLE_BUCKET="$(circleci env subst "${ORB_STR_BUNDLE_BUCKET}")"
+ORB_STR_BUNDLE_KEY="$(circleci env subst "${ORB_STR_BUNDLE_KEY}")"
+ORB_STR_REGION="$(circleci env subst "${ORB_STR_REGION}")"
+ORB_STR_ARGUMENTS="$(circleci env subst "${ORB_STR_ARGUMENTS}")"
+ORB_STR_PROFILE_NAME="$(circleci env subst "${ORB_STR_PROFILE_NAME}")"
+ORB_EVAL_BUNDLE_SOURCE="$(eval echo "${ORB_EVAL_BUNDLE_SOURCE}")"
 
-if [ -n "${ORB_EVAL_ARGUMENTS}" ]; then 
-    set -- "$@" "${ORB_EVAL_ARGUMENTS}"
+if [ -n "${ORB_STR_ARGUMENTS}" ]; then 
+    set -- "$@" "${ORB_STR_ARGUMENTS}"
 fi 
 
 aws deploy push \
-    --application-name "${ORB_EVAL_APPLICATION_NAME}" \
+    --application-name "${ORB_STR_APPLICATION_NAME}" \
     --source "${ORB_EVAL_BUNDLE_SOURCE}" \
-    --profile "${ORB_EVAL_PROFILE_NAME}" \
-    --region "${ORB_EVAL_REGION}" \
-    --s3-location s3://"${ORB_EVAL_BUNDLE_BUCKET}/${ORB_EVAL_BUNDLE_KEY}.${ORB_VAL_BUNDLE_TYPE}" "$@"
+    --profile "${ORB_STR_PROFILE_NAME}" \
+    --region "${ORB_STR_REGION}" \
+    --s3-location s3://"${ORB_STR_BUNDLE_BUCKET}/${ORB_STR_BUNDLE_KEY}.${ORB_VAL_BUNDLE_TYPE}" "$@"
